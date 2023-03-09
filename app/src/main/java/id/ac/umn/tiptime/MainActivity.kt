3ddf40e286ffa73da843e73e75b75fe971eec104
auddl <10horysheet10@gmail.com>
2023-03-09 10:44:34 +0700

    Creating Layout

--- app/src/main/java/id/ac/umn/tiptime/MainActivity.kt
+++ app/src/main/java/id/ac/umn/tiptime/MainActivity.kt
@@ -2,10 +2,17 @@ package id.ac.umn.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.ac.umn.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+//    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        binding.calculateButton.setOnClickListener{ calculateTip() }
     }
 }
